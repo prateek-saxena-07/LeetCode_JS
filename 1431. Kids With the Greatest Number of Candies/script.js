d881236086b5e3a32de3cae6142849184b2f8b5c
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-06-02 04:49:57 +0530

    update today's LeetCode Questions

---
+++ 1431. Kids With the Greatest Number of Candies/script.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  // const sortedCandies=candies.toSorted(function(a, b){return a-b});//function(a, b){return a-b} is used other wise it will sort the array in alphabetically as strings :(.
+  // console.log(sortedCandies);
+  // console.log(sortedCandies[sortedCandies.length-1])
+  const Max = Math.max(...candies);
+  const ans = [];
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= Max) {
+      ans[i] = true;
+    }
+    // console.log((candies[i]+extraCandies)>=(sortedCandies[sortedCandies.length-1]))}
+    else {
+      ans[i] = false;
+    }
+  }
+
+  return ans;
+};
