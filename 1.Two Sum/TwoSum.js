27568d2a1b797fcd702ed76ef4a40b2ca552ab74
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-06-04 07:16:48 +0530

    Two_sum

---
+++ 1.Two Sum/TwoSum.js
@@ -0,0 +1,12 @@
+var twoSum = function (nums, target) {
+  const pairIdx = {}; //dictionary
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (target - num in pairIdx) {
+      return [pairIdx[target - num], i];
+    }
+
+    pairIdx[num] = i;
+  }
+};
